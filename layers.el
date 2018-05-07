5b3f7a88bd81a868c0877e1413e6eab9adadcd73
timor <timor.dd@googlemail.com>
2018-05-08 01:03:59 +0200

    move config to corresponding places in layer

--- layers.el
+++ layers.el
@@ -1,4 +1,5 @@
 (configuration-layer/remove-layer 'helm)
-(configuration-layer/declare-layers '((ivy :variables ivy-initial-inputs-alist nil)
+(configuration-layer/declare-layers '(
+                                      (ivy :variables ivy-initial-inputs-alist nil)
                                       (shell :variables shell-default-shell 'multi-term)
                                       ))
--- packages.el
+++ packages.el
@@ -1,1 +1,32 @@
-(defconst timor-packages '())
+(defconst timor-packages '(
+                           exwm
+                           winner
+                           evil-lisp-state
+                           ivy
+                           projectile
+                           company
+                           ))
+
+(defun timor/post-init-exwm ()
+  (spacemacs/exwm-bind-switch-to-or-run-command "s-f" "Firefox" "firefox")
+  (spacemacs/exwm-bind-command "<XF86MonBrightnessUp>" "xbacklight -inc 5")
+  (spacemacs/exwm-bind-command "<XF86MonBrightnessDown>" "xbacklight -dec 5")
+  (exwm-input-set-key (kbd "<s-tab>") 'spacemacs/alternate-buffer))
+
+(defun timor/post-init-evil-lisp-state ()
+  (define-key evil-lisp-state-map (kbd "SPC") spacemacs-default-map))
+
+(defun timor/post-init-winner ()
+  (dolist (n '("*Help*" "*inferior-lisp*"))
+    (delete n winner-boring-buffers)))
+
+(defun timor/post-init-ivy ()
+  (setq ivy-initial-inputs-alist '()))
+
+(defun timor/post-init-projectile ()
+  (spacemacs/set-leader-keys "ps" 'projectile-save-project-buffers)
+  )
+
+(defun timor/post-init-company ()
+  (spacemacs|disable-company 'eshell-mode)
+  )
