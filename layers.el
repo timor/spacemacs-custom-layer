c2a5b530c98868983755b493382e083356fdf427
timor <timor.dd@googlemail.com>
2018-05-08 01:08:51 +0200

    ivy wrap

--- layers.el
+++ layers.el
@@ -1,5 +1,8 @@
 (configuration-layer/remove-layer 'helm)
 (configuration-layer/declare-layers '(
-                                      (ivy :variables ivy-initial-inputs-alist nil)
+                                      (ivy :variables
+                                           ivy-initial-inputs-alist nil
+                                           ivy-wrap t
+                                           )
                                       (shell :variables shell-default-shell 'multi-term)
                                       ))
