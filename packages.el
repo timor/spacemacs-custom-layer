145801a390af16542155e5e425a5c5665b06e675
timor <timor.dd@googlemail.com>
2019-01-26 10:25:03 +0100

    fix lazy loading

--- packages.el
+++ packages.el
@@ -14,11 +14,14 @@
   (exwm-input-set-key (kbd "<s-tab>") 'spacemacs/alternate-buffer))
 
 (defun timor/post-init-evil-lisp-state ()
-  (define-key evil-lisp-state-map (kbd "SPC") spacemacs-default-map))
+  (with-eval-after-load 'evil-lisp-state
+    (define-key evil-lisp-state-map (kbd "SPC") spacemacs-default-map)
+  )
 
 (defun timor/post-init-winner ()
-  (dolist (n '("*Help*" "*inferior-lisp*"))
-    (delete n winner-boring-buffers)))
+  (with-eval-after-load 'winner
+    (dolist (n '("*Help*" "*inferior-lisp*"))
+      (delete n winner-boring-buffers))))
 
 (defun timor/post-init-ivy ()
   (setq ivy-initial-inputs-alist '()))
